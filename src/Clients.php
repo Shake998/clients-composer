cdf79161e6f821e6a2e2c6fe3a0df5ef30c689e8
Shake998 <davidmasip16@gmail.com>
2020-05-19 00:43:39 +0200

    Add files via upload

--- src/Clients.php
+++ src/Clients.php
@@ -9,7 +9,7 @@
 
 // If you don't to add a custom vendor folder, then use the simple class
 // namespace HelloComposer;
-namespace ourcodeworld\ClientsComposer;
+namespace Shake998\ClientsComposer;
 
 class Clients
 {
